a5b460a3791a7937ca1aa6191c8305ffb8474be8
Saeid Gholami <saeidgholami101@gmail.com>
2020-09-20 22:32:41 +0430

    create frame for each part of app

--- main.py
+++ main.py
@@ -4,56 +4,55 @@ import tkinter.ttk as ttk
 
 
 class MenuView(ttk.Frame):
-	def __init__(self, parent, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		self["padding"] = (10, 10)
-		self["relief"] = "solid"
-		ttk.Button(self, text="Menu Items").pack()
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self["relief"] = "groov"
+        self["padding"] = 10
+        # this will be menu
+        ttk.Label(self, text="Menu Items goes here!").pack()
 
 
 class PdfView(ttk.Frame):
-	def __init__(self, parent, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		ttk.Button(self, text="Pdf Preview").pack(side=tk.LEFT, fill=tk.BOTH)
-		self["padding"] = (10, 10)
-		self["relief"] = "solid"
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self["relief"] = "solid"
+        self["padding"] = 10
+        ttk.Label(self, text="In Center Frame").pack()
 
-class ToolsView(ttk.Frame):
-	def __init__(self, parent, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		ttk.Button(self, text="Tools View").pack(side=tk.RIGHT, fill=tk.BOTH)
-		self["padding"] = (10, 10)
-		self["relief"] = "solid"
+
+
+class OpFrame(ttk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self["relief"] = "sunken"
+        self["padding"] = 10
+        ttk.Label(self, text="In Left Frame").grid()
 
 class MainView(ttk.Frame):
-	def __init__(self, parent, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		print(str(self))
-		print(str(parent))
-		self["padding"] = (5, 10)
-		# Frames
-		MenuView(self).grid(sticky=(tk.E, tk.W), columnspan=2)
-		PdfView(self).grid(row=1, column=0, sticky=(tk.E, tk.W, tk.S, tk.N))
-		ToolsView(self).grid(row=1, column=1, sticky=(tk.E, tk.W, tk.S, tk.N))
-		# 
-		# self.columnconfigure(0, weight=1)
-		# self.rowconfigure(0, weight=1)
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self["padding"] = 10
+        self["relief"] = "solid"
+
+        MenuView(self).grid(row=0, column=0, sticky=(tk.E, tk.W), columnspan=2)
+        PdfView(self).grid(row=1, column=0, sticky=(tk.S, tk.N, tk.E, tk.W))
+        OpFrame(self).grid(row=1, column=1, sticky=(tk.S, tk.N, tk.E, tk.W))
+
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=3)
+        self.columnconfigure(1, weight=1)
 
 
-class Application(tk.Tk):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		print(str(self))
-		self.title("PDF Editor")
-		self.geometry("200x300")
-		MainView(self).grid(sticky=(tk.S, tk.E, tk.N, tk.W))
-		self.columnconfigure(0, weight=1)
-		self.columnconfigure(1, weight=1)
-		# self.columnconfigure(1, weight=1)
-		self.rowconfigure(1, weight=1)
+
+class MyApplication(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Hello Tkinter")
+        self.geometry("400x300")
+        MainView(self).pack(expand=True, fill='both')
+        self.columnconfigure(0, weight=1)
 
 
 if __name__ == "__main__":
-	app = Application()
-	app.mainloop()
-
+    app = MyApplication()
+    app.mainloop()
